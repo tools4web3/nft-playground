c5b604d3dc4f7c2af8c946fe430e056ab945ab4c
damzaky <dmzsylvester@gmail.com>
2022-04-28 11:45:59 +0700

    finish signer

--- src/App.js
+++ src/App.js
@@ -24,8 +24,61 @@ function App() {
   const [provider, setProvider] = useState(null)
   const [isLoggedIn, setIsloggedIn] = useState(false)
 
-  function submit(e) {
+  const [output, setOutput] = useState("")
+
+  const stringToBoolean = (string) => 
+  string === 'false' || string === 'undefined' || string === 'null' || string === '0' ?
+  false : !!string
+
+  async function signData() {
+    const domain = {
+      name,
+      version,
+      chainId: parseInt(chainId),
+      verifyingContract,
+    }
+
+    const types = {
+      Primary: [
+        {name:"Signer",type:"address"},
+        {name:"IsListing",type:"bool"},
+        {name:"Nonce", type:"uint"},
+        {name:"CollectionAddress", type:"address"},
+        {name:"TokenID", type:"uint256"},
+        {name:"Price", type:"uint256"},
+        {name:"CurrencyAddress", type:"address"},
+        {name:"StartedAt", type:"uint256"},
+        {name:"ExpiredAt", type:"uint256"},
+        {name:"minSellerReceived", type:"uint"},
+        {name:"signature", type:"string"}
+      ],
+    }
+
+    const value = {
+      Signer,
+      IsListing: stringToBoolean(IsListing),
+      Nonce: parseInt(Nonce),
+      CollectionAddress,
+      TokenID: parseInt(TokenID),
+      Price: parseInt(Price),
+      CurrencyAddress,
+      StartedAt: parseInt(StartedAt),
+      ExpiredAt: parseInt(ExpiredAt),
+      minSellerReceived: parseInt(minSellerReceived),
+      signature,
+    }
+
+    const signer = await provider.getSigner()
+
+    const outputSignature = await signer._signTypedData(domain, types, value)
+
+    setOutput(outputSignature)
+  }
+
+  async function submit(e) {
     e.preventDefault()
+
+    await signData()
   }
 
   async function connect() {
@@ -39,10 +92,15 @@ function App() {
     setAccounts(_accounts)
   }
 
+  async function setChain() {
+    const net = await provider.getNetwork();
+    setchainId(net.chainId)
+  }
+
   useEffect(() => {
     if (provider) {
       checkLogin()
-      
+      setChain()
     }
   }, [provider]);
 
@@ -58,42 +116,38 @@ function App() {
       "any"
     );
     setProvider(provider)
-    // const signature = await signer._signTypedData(domain, types, value);
   }, [])
 
   return (
     <div className="App">
       {
-        !isLoggedIn ? <button onClick={() => connect()}>Connect</button> : `logged in as ${accounts[0]}`
+        !isLoggedIn ? <button onClick={() => connect()}>Connect</button> : <h2>{`logged in as ${accounts[0]}`}</h2>
       }
       
       <h1>Signer</h1>
       <form>
         <h2>Domain</h2>
-        <input type="text" placeholder="name" value={name} onChange={e => setname(e.target.value)} />
-        <input type="text" placeholder="version" value={version} onChange={e => setversion(e.target.value)} />
-        <input type="text" placeholder="chainId" value={chainId} onChange={e => setchainId(e.target.value)} />
-        <input type="text" placeholder="verifyingContract" value={verifyingContract} onChange={e => setverifyingContract(e.target.value)} />
+        <input type="text" placeholder="name (string)" value={name} onChange={e => setname(e.target.value)} />
+        <input type="text" placeholder="version (string)" value={version} onChange={e => setversion(e.target.value)} />
+        <input type="text" placeholder="chainId (must be the same as current net)" value={chainId} onChange={e => setchainId(e.target.value)} />
+        <input type="text" placeholder="verifyingContract (address, like 0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC)" value={verifyingContract} onChange={e => setverifyingContract(e.target.value)} />
 
         <h2>Content</h2>
-        <input type="text" placeholder="name" value={name} onChange={e => setname(e.target.value)} />
-        <input type="text" placeholder="version" value={version} onChange={e => setversion(e.target.value)} />
-        <input type="text" placeholder="chainId" value={chainId} onChange={e => setchainId(e.target.value)} />
-        <input type="text" placeholder="verifyingContract" value={verifyingContract} onChange={e => setverifyingContract(e.target.value)} />
-        <input type="text" placeholder="Signer" value={Signer} onChange={e => setSigner(e.target.value)} />
-        <input type="text" placeholder="IsListing" value={IsListing} onChange={e => setIsListing(e.target.value)} />
-        <input type="text" placeholder="Nonce" value={Nonce} onChange={e => setNonce(e.target.value)} />
-        <input type="text" placeholder="CollectionAddress" value={CollectionAddress} onChange={e => setCollectionAddress(e.target.value)} />
-        <input type="text" placeholder="TokenID" value={TokenID} onChange={e => setTokenID(e.target.value)} />
-        <input type="text" placeholder="Price" value={Price} onChange={e => setPrice(e.target.value)} />
-        <input type="text" placeholder="CurrencyAddress" value={CurrencyAddress} onChange={e => setCurrencyAddress(e.target.value)} />
-        <input type="text" placeholder="StartedAt" value={StartedAt} onChange={e => setStartedAt(e.target.value)} />
-        <input type="text" placeholder="ExpiredAt" value={ExpiredAt} onChange={e => setExpiredAt(e.target.value)} />
-        <input type="text" placeholder="minSellerReceived" value={minSellerReceived} onChange={e => setminSellerReceived(e.target.value)} />
-        <input type="text" placeholder="signature" value={signature} onChange={e => setsignature(e.target.value)} />
+        <input type="text" placeholder="Signer (address, like 0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC)" value={Signer} onChange={e => setSigner(e.target.value)} />
+        <input type="text" placeholder="IsListing (bool)" value={IsListing} onChange={e => setIsListing(e.target.value)} />
+        <input type="text" placeholder="Nonce (uint)" value={Nonce} onChange={e => setNonce(e.target.value)} />
+        <input type="text" placeholder="CollectionAddress (address, like 0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC)" value={CollectionAddress} onChange={e => setCollectionAddress(e.target.value)} />
+        <input type="text" placeholder="TokenID (uint256)" value={TokenID} onChange={e => setTokenID(e.target.value)} />
+        <input type="text" placeholder="Price (uint256)" value={Price} onChange={e => setPrice(e.target.value)} />
+        <input type="text" placeholder="CurrencyAddress (address, like 0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC)" value={CurrencyAddress} onChange={e => setCurrencyAddress(e.target.value)} />
+        <input type="text" placeholder="StartedAt (uint256)" value={StartedAt} onChange={e => setStartedAt(e.target.value)} />
+        <input type="text" placeholder="ExpiredAt (uint256)" value={ExpiredAt} onChange={e => setExpiredAt(e.target.value)} />
+        <input type="text" placeholder="minSellerReceived (uint)" value={minSellerReceived} onChange={e => setminSellerReceived(e.target.value)} />
+        <input type="text" placeholder="signature (string)" value={signature} onChange={e => setsignature(e.target.value)} />
         <br></br>
         <button type='submit' onClick={e => submit(e)}>Submit</button>
       </form>
+      <h2>OUTPUT SIGNATURE: { output }</h2>
     </div>
   );
 }
